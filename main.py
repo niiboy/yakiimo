ac27e917d8ccd4fb384033d043021d60a6f4e216
niiboy <niibo.hirofumi@gmail.com>
2019-12-25 12:03:23 +0900

    optionを追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import random
-
+from argparse import ArgumentParser
+from time import sleep
 
 class Dice():
     def __init__(self, face):
@@ -11,10 +12,12 @@ class Dice():
 
 
 class ButtleSystem():
-    def __init__(self, players):
+    def __init__(self, players, option):
         self.players = players
         self.hero = players[0]
         self.enemy = players[1]
+        self.luck_mode = option.luck
+        self.slow_mode = option.slow
 
     def dice_roll(self, face, times):
         dice = Dice(face)
@@ -30,19 +33,21 @@ class ButtleSystem():
         print(f"enemy has {enemy_tech} tech pt.")
 
         if hero_tech < enemy_tech:
-            print("hero has damage")
+            print("hero has damage!")
             self.hero.get_damage()
         elif enemy_tech < hero_tech:
             print("enemy has damage")
             self.enemy.get_damage()
         else:
-            print("dorrow!")
+            print("draw!\n")
 
-        print("")
 
     def buttle(self):
         while min(self.hero.get_hp(), self.enemy.get_hp()) > 0:
             self.judge()
+            if self.slow_mode is True:
+                sleep(2)
+
 
 
 class Player():
@@ -62,6 +67,13 @@ class Player():
 
         return status
 
+    def set_luck(self):
+        l = input(f"input status {self.name}. luck point >> ")
+        self.status["luck"] = int(l)
+
+    def loss_luck(self):
+        self.status["luck"] -= 1
+
     def get_hp(self):
         return self.status["hp"]
 
@@ -73,7 +85,7 @@ class Player():
 
     def get_damage(self):
         self.status["hp"] -= 2
-        print(f"{self.name} has {self.status['hp']} hp!")
+        print(f"{self.name} has {self.status['hp']} hp!\n")
         if self.status["hp"] <= 0:
             print(f"{self.name} has dead!")
 
@@ -86,9 +98,22 @@ def nake_players():
     players.append(enemy)
     return players
 
+def get_option():
+    argparser = ArgumentParser()
+    argparser.add_argument('-s', '--slow',
+                            action="store_true",
+                            help='use slow mode.')
+
+    argparser.add_argument('-l', '--luck',
+                            action="store_true",
+                            help='confirm the luck point.')
+
+    return argparser.parse_args()
+
 def main():
+    option = get_option()
     players = nake_players()
-    buttle_system = ButtleSystem(players)
+    buttle_system = ButtleSystem(players, option)
     buttle_system.buttle()
 
 
